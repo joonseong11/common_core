ac65e69c3c9a701e4b44fadf4fe835171628b00c
joonseong11 <jwjsjwnsstjd@gmail.com>
2022-02-27 00:42:52 +0900

    printf 03 try

--- printf/include/ft_printf.h
+++ printf/include/ft_printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jujeon <jujeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 19:48:56 by jujeon            #+#    #+#             */
-/*   Updated: 2022/02/06 19:48:57 by jujeon           ###   ########.fr       */
+/*   Updated: 2022/02/25 19:59:39 by jujeon           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 int	ft_printf(const char *format, ...);
 int	ft_printf_char(char c);
 int	ft_printf_str(char *s);
-int	ft_printf_ptr(unsigned long long nbr, const char *base);
+int	ft_printf_ptr(void *ptr, const char *base);
 int	ft_printf_nbr(int n);
 int	ft_printf_unbr(unsigned int n);
 int	ft_printf_nbrbase(unsigned int nbr, const char *base);
--- printf/sources/ft_printf_ptr.c
+++ printf/sources/ft_printf_ptr.c
@@ -6,15 +6,15 @@
 /*   By: jujeon <jujeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 15:59:40 by jujeon            #+#    #+#             */
-/*   Updated: 2022/02/25 18:06:03 by jujeon           ###   ########seoul.kr  */
+/*   Updated: 2022/02/25 20:34:48 by jujeon           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-void	ft_printf_ptrbase(unsigned long long num, const char *base, int *p)
+void	ft_printf_ptrbase(unsigned long num, const char *base, int *p)
 {
-	int			len;
+	unsigned long	len;
 
 	len = 0;
 	while (base[len] != 0)
@@ -31,17 +31,19 @@ void	ft_printf_ptrbase(unsigned long long num, const char *base, int *p)
 	}
 }
 
-int	ft_printf_ptr(unsigned long long nbr, const char *base)
+int	ft_printf_ptr(void *nbr, const char *base)
 {
-	int	i;
-	int	*p;
+	int				i;
+	int				*p;
+	unsigned long	nbr1;
 
 	i = 2;
 	p = &i;
+	nbr1 = (unsigned long)nbr;
 	ft_printf_str("0x");
 	if (base_check(base) == 1)
 	{
-		ft_printf_ptrbase(nbr, base, p);
+		ft_printf_ptrbase(nbr1, base, p);
 	}
 	else
 		return (0);
