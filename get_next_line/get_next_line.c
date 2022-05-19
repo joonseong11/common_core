74091d043cee6e6181d57f1ba2203bdbdb2b0142
joonseong11 <jwjsjwnsstjd@gmail.com>
2022-05-19 21:58:42 +0900

    5/19 cluster

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,30 +1,31 @@
 #include "get_next_line.h"
 
-char	*get_newline(char *str, )
+char	*get_next_buf(int	fd, char *temp)
 {
+	ssize_t	check;
+	char	*buf;
 
-	return ();
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (buf == 0)
+		return (NULL);
+	buf[0] = '\0';
+	check = read(fd, temp, BUFFER_SIZE);
+	if (check < 0)
+		return (NULL);
+	
 }
 
-char	*get_leftline()
+char	*get_next_line(int	fd)
 {
-	return ();
-}
+	static char	*temp;
+	char		*line;
 
-char	*get_next_line(int fd)
-{
-	static char	*temp[OPEN_MAX];
-	char		*buf;
-
-	buf = ft_calloc();
-	while (!istherenewline(temp[fd]))
-	{
-		r = read(fd, buf, BUFFER_SIZE);
-		buf[r] = '\0';
-		temp[fd] = strjoin(temp[fd], buf[r])
-	}
-	line = malloc(sizeof(char) * BUFFER_SIZE);
-	line = get_newline(temp[fd]);
-	temp[fd] = get_leftline(temp[fd],);
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	temp = get_next_buf(fd, temp);
+	if (temp == 0)
+		return (NULL);
+	line = parsing(temp);
+	temp = leave_after_NULL(temp);
 	return (line);
 }
