1ad2a2f8ff71f126ea7c8b31a2d75e156eaf8429
joonseong11 <jwjsjwnsstjd@gmail.com>
2022-08-29 21:06:11 +0900

    refactoring 01

--- philosophers/philo_bonus/philo_bonus.c
+++ philosophers/philo_bonus/philo_bonus.c
@@ -6,160 +6,15 @@
 /*   By: jujeon <jujeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 01:21:49 by jujeon            #+#    #+#             */
-/*   Updated: 2022/08/25 15:59:17 by jujeon           ###   ########.fr       */
+/*   Updated: 2022/08/29 20:58:25 by jujeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-size_t get_time()
+long long	p_atoi(const char *str)
 {
-	struct timeval time;
-
-	if (gettimeofday(&time, NULL) == ERROR)
-		return (ERROR);
-	return (time.tv_sec * 1000 + time.tv_usec / 1000);
-}
-
-void smart_timer(size_t time)
-{
-	size_t start;
-
-	start = get_time();
-	// if (start == ERROR)
-	// return (ERROR);
-	while (get_time() - start < time)
-		usleep(100);
-}
-
-void *eat_checker(void *param)
-{
-	int i;
-	t_philo *philo;
-
-	philo = (t_philo *)param;
-	i = -1;
-	while (++i < philo->info.arg.n_philo)
-	{
-		sem_wait(philo->info.sema.eat_checker);
-		if (i + 1 == philo->info.arg.n_philo)
-			break;
-		sem_post(philo->info.sema.print);
-	}
-	// sem_wait(philo->info.sema.print);
-	i = -1;
-	while (++i < philo->info.arg.n_philo)
-		kill(philo->info.pid[i], SIGKILL);
-	// sem_post(philo->info.sema.print);			// 세마포어 종료 및 할당한 자원 해제
-	sem_close(philo->info.sema.eat_checker); // 세마포어 종료 및 할당한 자원 해제
-	sem_unlink("eat_checker");				 // 세마포어 객체 해제 제거
-	return (NULL);
-}
-
-void philo_print(t_philo *philo, t_info *info, int idx, char *str)
-{
-	sem_wait(philo->info.sema.print);
-	printf("%ld %d %s\n", get_time() - info->birth_t, idx + 1, str);
-	if (strstr(str, "is eating"))
-	{
-		philo->last_eat_t = get_time();
-		if (++(philo->eat_cnt) == info->arg.must_eat)
-		{
-			sem_post(info->sema.eat_checker);
-			return;
-		}
-	}
-	sem_post(philo->info.sema.print);
-}
-
-int take_fork(t_philo *philo)
-{
-	sem_wait(philo->info.sema.fork);
-	philo_print(philo, &philo->info, philo->idx, "has taken a fork");
-	sem_wait(philo->info.sema.fork);
-	philo_print(philo, &philo->info, philo->idx, "has taken a fork");
-	return (SUCCESS);
-}
-
-int eating(t_philo *philo)
-{
-	philo_print(philo, &philo->info, philo->idx, "is eating");
-	smart_timer((*philo).info.arg.eat_time);
-	return (SUCCESS);
-}
-
-int sleep_thinking(t_philo *philo)
-{
-	sem_post(philo->info.sema.fork);
-	sem_post(philo->info.sema.fork);
-	philo_print(philo, &philo->info, philo->idx, "is sleeping");
-	smart_timer(philo->info.arg.sleep_time);
-	philo_print(philo, &philo->info, philo->idx, "is thinking");
-	return (SUCCESS);
-}
-
-void *monitor(void *param)
-{
-	size_t now_t;
-
-	t_philo *const philo = (t_philo *)param;
-
-	while (1)
-	{
-		sem_wait(philo->info.sema.print);
-		now_t = get_time();
-		if (now_t > philo->info.arg.die_time + philo->last_eat_t)
-		{
-			printf("%lu %d died\n", now_t - (*philo).info.birth_t, philo->idx + 1);
-			exit(1);
-		}
-		sem_post(philo->info.sema.print);
-	}
-	return (NULL);
-}
-
-void action(t_philo philo)
-{
-	pthread_t tid;
-
-	tid = NULL;
-	// pthread_create(&tid, NULL, monitor, &philo);
-	//  sem_wait(philo.info.sema.print);
-	philo.last_eat_t = get_time();
-	pthread_create(&tid, NULL, monitor, &philo);
-	// sem_post(philo.info.sema.print);
-	while (!take_fork(&philo) && !eating(&philo) && !sleep_thinking(&philo))
-		;
-	exit(1);
-}
-
-int init_philo(t_philo *philo, t_info *info, t_arg *arg)
-{
-	(*philo).last_eat_t = get_time();
-	info->sema.fork = sem_open("sem_fork", O_CREAT | O_EXCL, 0644, arg->n_philo);
-	if (info->sema.fork == SEM_FAILED)
-	{
-		sem_unlink("sem_fork");
-		info->sema.fork = sem_open("sem_fork", O_CREAT | O_EXCL, 0644, arg->n_philo);
-	}
-	info->sema.print = sem_open("sem_print", O_CREAT | O_EXCL, 0644, 1);
-	if (info->sema.print == SEM_FAILED)
-	{
-		sem_unlink("sem_print");
-		info->sema.print = sem_open("sem_print", O_CREAT | O_EXCL, 0644, 1);
-	}
-	info->sema.eat_checker = sem_open("eat_checker", O_CREAT | O_EXCL, 0644, 0);
-	if (info->sema.eat_checker == SEM_FAILED)
-	{
-		sem_unlink("eat_checker");
-		info->sema.eat_checker = sem_open("eat_checker", O_CREAT | O_EXCL, 0644, 0);
-	}
-	return (SUCCESS);
-}
-
-long long p_atoi(const char *str)
-{
-	long long ret;
+	long long	ret;
 
 	ret = 0;
 	while (*str)
@@ -178,25 +33,12 @@ long long p_atoi(const char *str)
 }
 
 /*
-	> p_atoi memo <
-	- when get negetive number
-	- when get character
-	- when get super big number
-	return (-1) <----------------
-
-	> must_eat memo <
-	0 means no argument of 6
-	when get -1 means error
-	{
-		error cases are
-		- input of must_eat is 0 (need to add)
-		- input of must_eat is negetive (p_atoi)
-		- input of must_eat is combine of characters (p_atoi)
-		- input of must_eat is super big number (p_atoi)
-	}
+	error cases are
+	- input of must_eat is 0, negative, combine of characters or super big number
+	- then p_atoi memo return -1
 */
 
-int parse_arg(int argc, char **argv, t_info *info)
+int	parse_arg(int argc, char **argv, t_info *info)
 {
 	if (!(argc == 5 || argc == 6))
 		return (ERROR);
@@ -219,33 +61,20 @@ int parse_arg(int argc, char **argv, t_info *info)
 	return (SUCCESS);
 }
 
-int main(int argc, char **argv)
+int	init(int argc, char **argv, t_philo **philo)
 {
-	t_philo	philo;
+	*philo = malloc(sizeof(t_philo) * (*philo)->info.arg.n_philo);
+	memset(*philo, 0, sizeof(t_philo));
+	if (parse_arg(argc, argv, &(*philo)->info) == ERROR)
+		return (ERROR);
+	return (SUCCESS);
+}
 
-	if (parse_arg(argc, argv, &philo.info) == ERROR)
+int	main(int argc, char **argv)
+{
+	t_philo	*philo;
+
+	if (init(argc, argv, &philo))
 		return (ERROR);
-	if (init_philo() == ERROR)
-		return (ERROR);
-	if (argc == 6)
-	{
-		pthread_create(&tid_eatchecker, NULL, eat_checker, &philo);
-		pthread_detach(tid_eatchecker);
-	}
-	waitpid(-1, 0, 0);
-	// sem_post(philo.info.sema.print);
-	idx = -1;
-	while (++idx < philo.info.arg.n_philo)
-	{
-		kill(pid[idx], SIGKILL);
-		// printf("%d\n", pid[idx]);
-	}
-	sem_close(philo.info.sema.fork);  // 세마포어 종료 및 할당한 자원 해제
-	sem_close(philo.info.sema.print); // 세마포어 종료 및 할당한 자원 해제
-	sem_unlink("sem_fork");			  // 세마포어 객체 해제 제거
-	sem_unlink("sem_print");		  // 세마포어 객체 해제 제거
-	free(pid);
-	while (idx--) // n - 1번
-		waitpid(-1, 0, 0);
 	return (0);
 }
