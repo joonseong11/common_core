7b301da666109fba5070bb99a9209626aa3647bb
joonseong11 <jwjsjwnsstjd@gmail.com>
2022-08-30 21:30:06 +0900

    refactoring02

--- philosophers/philo_bonus/philo_bonus.c
+++ philosophers/philo_bonus/philo_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jujeon <jujeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 01:21:49 by jujeon            #+#    #+#             */
-/*   Updated: 2022/08/29 20:58:25 by jujeon           ###   ########.fr       */
+/*   Updated: 2022/08/30 21:18:16 by jujeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,18 +61,63 @@ int	parse_arg(int argc, char **argv, t_info *info)
 	return (SUCCESS);
 }
 
-int	init(int argc, char **argv, t_philo **philo)
+size_t	get_time(void)
 {
-	*philo = malloc(sizeof(t_philo) * (*philo)->info.arg.n_philo);
-	memset(*philo, 0, sizeof(t_philo));
-	if (parse_arg(argc, argv, &(*philo)->info) == ERROR)
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+int	init_philo(t_philo *philo, t_info *info, t_arg *arg)
+{
+	philo->last_eat_t = get_time();
+	info->sema.fork = sem_open("sem_fork", O_CREAT | O_EXCL, 0644, \
+		arg->n_philo);
+	if (info->sema.fork == SEM_FAILED)
+	{
+		sem_unlink("sem_fork");
+		info->sema.fork = sem_open("sem_fork", O_CREAT | O_EXCL, 0644, \
+			arg->n_philo);
+	}
+	info->sema.print = sem_open("sem_print", O_CREAT | O_EXCL, 0644, 1);
+	if (info->sema.print == SEM_FAILED)
+	{
+		sem_unlink("sem_print");
+		info->sema.print = sem_open("sem_print", O_CREAT | O_EXCL, 0644, 1);
+	}
+	info->sema.print = sem_open("eat_checker", O_CREAT | O_EXCL, 0644, 0);
+	if (info->sema.eat_checker == SEM_FAILED)
+	{
+		sem_unlink("eat_checker");
+		info->sema.eat_checker = sem_open("eat_checker", O_CREAT | O_EXCL, \
+			0644, 0);
+	}
+	return (SUCCESS);
+}
+
+//			O_EXCL          error if create and semaphore exists
+
+int	init(int argc, char **argv, t_philo *philo, pid_t *pid)
+{
+	int	idx;
+
+	memset(philo, 0, sizeof(t_philo));
+	if (parse_arg(argc, argv, &philo->info) == ERROR)
 		return (ERROR);
+	init_philo(philo, &philo->info, &philo->info.arg);
+	idx = 0;
+	philo->info.birth_t = get_time();
+	while (idx < philo->info.arg.n_philo)
+	{
+		pid[idx] = fork();
+	}
 	return (SUCCESS);
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo	*philo;
+	t_philo	philo;
 
 	if (init(argc, argv, &philo))
 		return (ERROR);
